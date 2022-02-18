b59eeeb12c54f02b455cef80fbfafb0747e8f0bd
guilhermeprcruz <99661923+guilhermeprcruz@users.noreply.github.com>
2022-02-18 11:08:06 -0300

    Add files via upload

---
+++ IFs.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>;
+int main (){
+int DDD;
+printf("Digite o DDD: ");
+scanf("%d", &DDD);
+
+if(DDD == 61)
+printf("Brasilia");
+
+else if (DDD == 71)
+printf ("Salvador");
+
+else if (DDD == 11)
+printf ("Sao Paulo");
+
+else if (DDD == 21)
+printf ("Rio de Janeiro");
+
+else if (DDD == 32)
+printf ("Juiz de fora");
+
+else if (DDD == 19)
+printf ("Campinas");
+
+else if (DDD == 27)
+printf ("Vitoria");
+
+else if (DDD == 31)
+printf("Belo Horizonte");
+else printf ("DDD nao encontrado");
+
+
+
+	
+	
+	
+	
+	
+	
+}
